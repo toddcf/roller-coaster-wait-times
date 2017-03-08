4353b897f4817b73b50d624d4baf8b0373073388
Todd Croak-Falen <toddcf@gmail.com>
2017-03-08 14:26:34 -0800

    Added line array and set active to false.

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,4 @@
-// Array to hold the people in line:
+// Array to hold the people in line. Initialized with 25 people.
+var line = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25];
+// Tracks whether the roller coaster is running or not. Iniitalized to false. (User will click button to get it started.)
+var active = false;
